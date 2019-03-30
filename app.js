40b5ce2d414febe984305e8ea9ea2c78feb0c3e5
Jacob Papageorgiou <jacob.papageorgiou@gmail.com>
2019-03-30 15:31:17 +0000

    Fixed bug with fade in pointer events all to make JS work

--- app.js
+++ app.js
@@ -13,8 +13,27 @@ const game = () => {
     });
   };
 
+  // Play match!
+  const playMatch = () => {
+    const options = document.querySelectorAll(".options button");
+    const playerHand = document.querySelector(".player-hand");
+    const computerHand = document.querySelector(".computer-hand");
+    // Computer's options - randomly generated
+    const computerOptions = ["rock", "paper", "scissors"];
+
+    options.forEach(option => {
+      option.addEventListener("click", function() {
+        // Computer's choice
+        const computerNumber = Math.floor(Math.random() * 3);
+        const computerChoice = computerOptions[computerNumber];
+        console.log(computerChoice);
+      });
+    });
+  };
+
   // Call the inner function to invoke it within the game function
   startGame();
+  playMatch();
 };
 
 // Call the game function to start the game
