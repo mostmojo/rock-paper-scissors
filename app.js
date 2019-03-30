d907ed34807ad56441eeb2d4f71dbf3412591d1a
Jacob Papageorgiou <jacob.papageorgiou@gmail.com>
2019-03-30 14:14:41 +0000

    Added querySelectors and EventListeners to code to fade screens in/out

---
+++ app.js
@@ -0,0 +1,21 @@
+const game = () => {
+  let pScore = 0;
+  let cScore = 0;
+
+  const startGame = () => {
+    const playBtn = document.querySelector(".intro button");
+    const introScreen = document.querySelector(".intro");
+    const match = document.querySelector(".match");
+
+    playBtn.addEventListener("click", () => {
+      introScreen.classList.add("fadeOut");
+      match.classList.add("fadeIn");
+    });
+  };
+
+  // Call the inner function to invoke it within the game function
+  startGame();
+};
+
+// Call the game function to start the game
+game();
